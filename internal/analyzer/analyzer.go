61e7776bec1b4a3c49494836b6b6a1ef32ef1bfe
Korovkina Ulyana <yliana.korovkina@gmail.com>
2026-03-15 12:02:44 +0300

    feat: add AST traversal

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,9 +1,13 @@
 package analyzer
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+)
 
 var Analyzer = &analysis.Analyzer{
-	Name: "loglint",
-	Doc:  "checks log messages formatting rules",
-	Run:  run,
+	Name:     "loglint",
+	Doc:      "checks log messages formatting rules",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
--- internal/analyzer/run.go
+++ internal/analyzer/run.go
@@ -1,7 +1,52 @@
 package analyzer
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"go/ast"
 
-func run(pass *analysis.Pass) (interface{}, error) {
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+func run(pass *analysis.Pass) (any, error) {
+	inspector, ok := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	if !ok {
+		return nil, nil
+	}
+
+	nodeFilter := []ast.Node{
+		(*ast.CallExpr)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(n ast.Node) {
+		call := n.(*ast.CallExpr)
+
+		selector, ok := call.Fun.(*ast.SelectorExpr)
+		if !ok {
+			return
+		}
+
+		if isLogMethod(selector.Sel.Name) {
+			pass.Reportf(call.Pos(), "log call detected: %s", selector.Sel.Name)
+		}
+	})
+
 	return nil, nil
+}
+
+func isLogMethod(name string) bool {
+
+	switch name {
+
+	case "Info",
+		"Warn",
+		"Error",
+		"Debug",
+		"Fatal",
+		"Panic":
+		return true
+
+	}
+
+	return false
 }
